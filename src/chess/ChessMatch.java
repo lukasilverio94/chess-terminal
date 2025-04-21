3ad9bdd44f211666408d456f41d67e64971d575b
lukasilverio <lukasilverio94@gmail.com>
2025-04-21 23:37:55 +0200

    feat: ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -9,31 +8,35 @@ import chess.pieces.Rook;
 // Rules for the game
 public class ChessMatch {
 
-    private Board board;
+    private final Board board;
 
-    public ChessMatch(){
+    public ChessMatch() {
         board = new Board(8, 8);
         initialSetup();
     }
 
     // retorna a matriz de pecas da nossa partida de xadrez
-    public ChessPiece[][] getPieces(){
+    public ChessPiece[][] getPieces() {
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
         // percorrrer matriz de pecas do tabuleiro
         // pra cada peca do tabuleiro, fazer um downcasting para ChessPiece[][]
-        for (int i = 0; i < board.getRows(); i++){
-            for (int j = 0; j < board.getColumns(); j++){
+        for (int i = 0; i < board.getRows(); i++) {
+            for (int j = 0; j < board.getColumns(); j++) {
                 mat[i][j] = (ChessPiece) board.piece(i, j);
             }
         }
         return mat;
     }
 
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
     // initialize the match placing the pieces on the board
-    private void initialSetup(){
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-        board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+    private void initialSetup() {
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
 
     }
 }
---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,43 @@
+package chess;
+
+import boardgame.BoardException;
+import boardgame.Position;
+
+public class ChessPosition {
+    /*
+    class for mapping from a matrix coordinates (n, n) to a combination of letters (a, b, c, d, e, f, g, h)
+    and numbers, representing rows/columns, respectively
+     */
+    private final char column;
+    private final int row;
+
+    public ChessPosition(char column, int row) {
+        if (column < 'a' || column > 'h' || row < 1 || row > 8){
+            throw new BoardException("Error instantiating ChessPosition. Valid values are form a1 to h8");
+        }
+        this.column = column;
+        this.row = row;
+    }
+
+    public char getColumn() {
+        return column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    protected Position toPosition(){
+        return new Position(8 - row, column - 'a');
+    }
+
+    // inverse from method above
+    protected static ChessPosition fromPosition(Position position){
+        return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+    }
+
+    @Override
+    public String toString(){
+        return "" + column + row;
+    }
+}
