c153755db8f487fea1cdec956b49ebf06fb92646
lukasilverio <lukasilverio94@gmail.com>
2025-04-24 15:30:38 +0200

    refactor: remove unnecessary comment

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -21,7 +21,6 @@ public class King extends ChessPiece {
 
     @Override
     public boolean[][] possibleMoves() {
-        // todo: improve this later
         boolean[][] matrix = new boolean[getBoard().getRows()][getBoard().getColumns()];
 
         Position p = new Position(0, 0);
